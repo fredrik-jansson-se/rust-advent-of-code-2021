7dc21e8a345643ed2549a5ee3fadb07a78ec5fc6
Fredrik Jansson <fredrik.jansson.se@gmail.com>
2021-12-04 11:46:19 +0100

    Day 4

--- src/aoc5.rs
+++ src/aoc5.rs
@@ -1,117 +1,23 @@
 use std::fs;
 
-#[derive(Debug)]
-enum Cmd {
-    Front,
-    Back,
-    Left,
-    Right,
-}
-
-impl std::convert::From<char> for Cmd {
-    fn from(c: char) -> Self {
-        match c {
-            'F' => Self::Front,
-            'B' => Self::Back,
-            'L' => Self::Left,
-            'R' => Self::Right,
-            _ => unreachable!(),
-        }
-    }
-}
-
-fn parse_cmds(i: &str) -> Vec<Vec<Cmd>> {
-    i.lines()
-        .map(|line| {
-            line.chars()
-                .map(std::convert::Into::into)
-                .collect::<Vec<_>>()
-        })
-        .collect()
-}
-
-fn boarding_pass(p: &[Cmd]) -> (usize, usize) {
-    let mut top_row = 128;
-    let mut low_row = 0;
-    assert_eq!(p.len(), 10);
-    for cmd in &p[0..7] {
-        // dbg! {(cmd, low_row, top_row)};
-        match cmd {
-            Cmd::Front => {
-                top_row -= (top_row - low_row) / 2;
-            }
-            Cmd::Back => {
-                low_row += (top_row - low_row) / 2;
-            }
-            _ => unreachable!(),
-        }
-    }
-    let mut max_seat = 8;
-    let mut min_seat = 0;
-    for cmd in &p[7..10] {
-        // dbg! {(cmd, min_seat, max_seat)};
-        match cmd {
-            Cmd::Right => {
-                min_seat += (max_seat - min_seat) / 2;
-            }
-            Cmd::Left => {
-                max_seat -= (max_seat - min_seat) / 2;
-            }
-            _ => unreachable!(),
-        }
-    }
-
-    // dbg! {(low_row, min_seat)}
-    (low_row, min_seat)
-}
-
-pub fn run() {
+pub fn run() -> anyhow::Result<()> {
     let input = fs::read_to_string("day5.txt").unwrap();
 
-    println!("5:1 {}", run_1(&input));
-    println!("5:2 {}", run_2(&input));
+    println!("5:1 {}", run_1(&input)?);
+    println!("5:2 {}", run_2(&input)?);
+    Ok(())
 }
 
-fn seat_id((row, seat): (usize, usize)) -> usize {
-    row * 8 + seat
+fn run_1(input: &str) -> anyhow::Result<usize> {
+    Ok(0)
 }
 
-fn run_1(input: &str) -> usize {
-    let bps = parse_cmds(input);
-
-    bps.iter()
-        .map(|bp| boarding_pass(bp))
-        .map(|rs| seat_id(rs))
-        .max()
-        .unwrap()
-}
-
-fn run_2(input: &str) -> usize {
-    let bps = parse_cmds(input);
-
-    let taken: Vec<usize> = bps
-        .iter()
-        .map(|bp| boarding_pass(bp))
-        .map(|rs| seat_id(rs))
-        .collect();
-    let first_seat_id: usize = 1 * 8 + 0;
-    let last_seat_id: usize = 126 * 8 + 7;
-    (first_seat_id..last_seat_id)
-        .find(|seat_id| {
-            taken.contains(&(seat_id - 1))
-                && !taken.contains(seat_id)
-                && taken.contains(&(seat_id + 1))
-        })
-        .unwrap()
+fn run_2(input: &str) -> anyhow::Result<usize> {
+    Ok(0)
 }
 
 #[cfg(test)]
 mod tests {
     #[test]
-    fn aoc5_run_1() {
-        assert_eq!(super::run_1("FBFBBFFRLR"), 357);
-        assert_eq!(super::run_1("BFFFBBFRRR"), 567);
-        assert_eq!(super::run_1("FFFBBBFRRR"), 119);
-        assert_eq!(super::run_1("BBFFBBFRLL"), 820);
-    }
+    fn aoc5_run_1() {}
 }
--- src/main.rs
+++ src/main.rs
@@ -19,8 +19,8 @@ mod aoc2;
 // mod aoc24;
 // mod aoc25;
 mod aoc3;
-// mod aoc4;
-// mod aoc5;
+mod aoc4;
+mod aoc5;
 // mod aoc6;
 // mod aoc7;
 // mod aoc8;
@@ -31,17 +31,14 @@ fn main() -> anyhow::Result<()> {
     let mut a = env::args();
     a.next();
 
-    let day = match a.next() {
-        Some(s) => usize::from_str_radix(&s, 10).unwrap(),
-        None => 0,
-    };
+    let day = a.next().and_then(|s| s.parse::<usize>().ok()).unwrap_or(0);
 
     match day {
         1 => aoc1::run(),
         2 => aoc2::run(),
         3 => aoc3::run(),
-        // 4 => aoc4::run(),
-        // 5 => aoc5::run(),
+        4 => aoc4::run(),
+        5 => aoc5::run(),
         // 6 => aoc6::run(),
         // 7 => aoc7::run(),
         // 8 => aoc8::run(),
