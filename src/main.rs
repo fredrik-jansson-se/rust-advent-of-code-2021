84b2db5efae1528690081bfe4b5900042545d00d
Fredrik Jansson <fredrik.jansson.se@gmail.com>
2021-12-05 09:16:27 +0100

    Day 5

--- src/main.rs
+++ src/main.rs
@@ -21,7 +21,7 @@ mod aoc2;
 mod aoc3;
 mod aoc4;
 mod aoc5;
-// mod aoc6;
+mod aoc6;
 // mod aoc7;
 // mod aoc8;
 // mod aoc9;
@@ -39,7 +39,7 @@ fn main() -> anyhow::Result<()> {
         3 => aoc3::run(),
         4 => aoc4::run(),
         5 => aoc5::run(),
-        // 6 => aoc6::run(),
+        6 => aoc6::run(),
         // 7 => aoc7::run(),
         // 8 => aoc8::run(),
         // 9 => aoc9::run(),
